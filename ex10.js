ee77c4b4cd048636836e3acddd4c3e9c1e47f03d
Jon Roby <jonathan.roby1@gmail.com>
2016-03-14 20:23:18 -0700

    ex11.js

--- ex10.js
+++ ex10.js
@@ -4,10 +4,13 @@ Array.prototype.concatAll = function() {
     subArray.forEach(function(item) {
       results.push(item);
     });
-
   });
   return results;
 }
 
 var test = [ [1,2,3], [4,5,6], [7,8,9] ].concatAll()
 console.log(test);
+
+
+
+  // console.log( movieLists.concatAll() );
---
+++ ex11.js
@@ -0,0 +1,76 @@
+Array.prototype.concatAll = function() {
+  var results = [];
+  this.forEach(function(subArray) {
+    subArray.forEach(function(item) {
+      results.push(item);
+    });
+  });
+  return results;
+}
+
+(function ex11() {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id:432534, time:65876586 }]
+					}
+				]
+			},
+			{
+				name: "Dramas",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id:432534, time:65876586 }]
+					}
+				]
+			}
+		];
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+	// Use map and concatAll to flatten the movieLists in a list of video ids.
+	// ------------   INSERT CODE HERE!  -----------------------------------
+
+
+
+  var result = movieLists.map(function(category) {
+      return category.videos.map(function (video) {
+        console.log(video.id);
+        return video.id;
+      });
+    });
+  console.log(result);
+  // console.log(result.concatAll());
+    // .concatAll();
+
+  console.log(result);
+	return result; // Complete this expression!
+
+})();
