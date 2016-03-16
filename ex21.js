c92b665e0fa3f5f525e060dd15905526b2f70101
Jon Roby <jonathan.roby1@gmail.com>
2016-03-15 19:33:08 -0700

    ex21.js

---
+++ ex21.js
@@ -0,0 +1,50 @@
+(function ex21() {
+	var videos = [
+			{
+				"id": 70111470,
+				"title": "Die Hard",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 4.0,
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 5.0,
+			},
+			{
+				"id": 65432445,
+				"title": "The Chamber",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 4.0,
+			},
+			{
+				"id": 675465,
+				"title": "Fracture",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 5.0,
+			}
+		],
+		bookmarks = [
+			{id: 470, time: 23432},
+			{id: 453, time: 234324},
+			{id: 445, time: 987834}
+		],
+	counter,
+	videoIdAndBookmarkIdPairs = [];
+
+	for(counter = 0; counter < Math.min(videos.length, bookmarks.length); counter++) {
+    var videoId = videos[counter].id;
+    var bookmarkId = bookmarks[counter].id;
+    videoIdAndBookmarkIdPairs.push({videoId: videoId, bookmarkId: bookmarkId});
+		// Insert code here to create a {videoId, bookmarkId} pair and add it to the videoIdAndBookmarkIdPairs array.
+	}
+
+  console.log(videoIdAndBookmarkIdPairs);
+	return videoIdAndBookmarkIdPairs;
+
+})();
